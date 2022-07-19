253bb89cbba34482a0ddee6f128b6201c188e9c7
jm <jm.dev.study@gmail.com>
2022-07-19 18:10:04 +0900

    ✨ Public page와 관련된 라우터 및 페이지 컴포넌트 생성
    Co-authored-by: mogooee <mogoo22@naver.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '@/styles/globalStyle';
 import theme from '@/styles/theme';
 
+import Routers from '@/router';
+
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <div>ff</div>
+      <Routers />
     </ThemeProvider>
   );
 };
