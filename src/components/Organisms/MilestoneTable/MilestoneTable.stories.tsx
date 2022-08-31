997b6a79661a02249229a6e12e5b44a9d781764b
jm <jm.dev.study@gmail.com>
2022-08-31 14:20:40 +0900

    💄 MilestoneTable 컴포넌트 UI 구현 및 스토리 추가 - searchParams에 따라 마일스톤의 상태를 구분한다. - 해당하는 상태의 마일스톤이 없을 경우 EmpthMilestoneItem 컴포넌트를 보여준다.

---
+++ src/components/Organisms/MilestoneTable/MilestoneTable.stories.tsx
@@ -0,0 +1,34 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import MilestoneTable, { MilestoneListTypes } from '@/components/Organisms/MilestoneTable';
+
+export default {
+  title: 'Organisms/MilestoneTable',
+  component: MilestoneTable,
+} as ComponentMeta<typeof MilestoneTable>;
+
+const Template: ComponentStory<typeof MilestoneTable> = (args) => <MilestoneTable {...args} />;
+
+const MILESTONE_DATA: MilestoneListTypes = {
+  openedMilestones: [
+    {
+      id: 0,
+      title: '마일스톤 1',
+      description: null,
+      dueDate: '2022-09-11',
+      closed: false,
+    },
+    {
+      id: 1,
+      title: '마일스톤 2',
+      description: '열린 마일스톤에 대한 설명',
+      dueDate: null,
+      closed: true,
+    },
+  ],
+  closedMilestones: [],
+};
+
+export const Initial = Template.bind({});
+Initial.args = {
+  milestoneData: MILESTONE_DATA,
+};
