cde4600f64fc096eafddd58813cf4c711740a86c
jm <jm.dev.study@gmail.com>
2022-08-31 14:31:41 +0900

    🤡 milestone에 대한 handler 추가 및 설정 - milestone get 요청에 대한 응답 추가

--- src/mocks/server.ts
+++ src/mocks/server.ts
@@ -1,5 +1,7 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { setupServer } from 'msw/node';
-import { authHandlers } from './handler/auth';
-import { labelHandlers } from './handler/label';
+import { milestoneHandlers } from '@/mocks/handlers/milestones';
+import { authHandlers } from '@/mocks/handlers/auth';
+import { labelHandlers } from '@/mocks/handlers/label';
 
-export const server = setupServer(...authHandlers, ...labelHandlers);
+export const server = setupServer(...authHandlers, ...labelHandlers, ...milestoneHandlers);
