08960cf5a706cd43176d8ca869dbdd5ff2f048b4
연 <mogoo22@naver.com>
2022-09-03 15:50:09 +0900

    ♻️ useOnceQuery 훅을 추가하여 silentLogin을 새로고침시 최초 한번만 실행되도록 함 - enabled 옵션과 appComponentDidMount 상태로 조건부 렌더링을 한다.

--- src/hooks/useLogin.ts
+++ src/hooks/useLogin.ts
@@ -1,33 +1,35 @@
-import { useQuery } from '@tanstack/react-query';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from 'recoil';
 import { LoginUserInfoState } from '@/stores/loginUserInfo';
 import { getUserInfo, silentRefresh } from '@/api/login_logout';
 import { MemeberResponseTypes } from '@/api/signUp';
+import OAuthState from '@/stores/auth';
+import useOnceQuery from '@/hooks/useOnceQuery';
 
 const useLogin = () => {
   const [loginUserInfo, setLoginUserInfo] = useRecoilState(LoginUserInfoState);
+  const setIsOAuth = useSetRecoilState(OAuthState);
 
   const onSuccessLogin = (userInfo: MemeberResponseTypes) => {
     localStorage.setItem('Authentication', 'true');
     setLoginUserInfo(userInfo);
+    setIsOAuth(true);
   };
 
   const saveLoginUserInfo = async () => {
     const userInfo = await getUserInfo();
-    localStorage.setItem('Authentication', 'true');
-    onSuccessLogin(userInfo);
+    saveAuthLoginState(userInfo);
   };
 
-  const silentLogin = (): boolean => {
-    const { data: isAuthorized } = useQuery<any>(['silentLogin'], silentRefresh, {
-      cacheTime: Infinity,
-      staleTime: Infinity,
+  const useSilentLogin = () => {
+    useOnceQuery(['silentLogin'], silentRefresh, {
+      enabled: !!localStorage.getItem('Authentication'),
+      onSuccess: () => {
+        onSuccessLogin();
+      },
+      onError: () => {
+        localStorage.removeItem('Authentication');
+      },
     });
-    if (!isAuthorized) {
-      localStorage.removeItem('Authentication');
-      return false;
-    }
-    return true;
   };
 
   return { loginUserInfo, saveLoginUserInfo, setLoginUserInfo, silentLogin, onSuccessLogin };
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,25 +1,22 @@
-import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import useLogin from '@/hooks/useLogin';
 
 import PrivateRouter from '@/router/PrivateRouter';
 import PublicRouter from '@/router/PublicRouter';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import OAuthState, { AppComponentMountState } from '@/stores/auth';
+import { useEffect } from 'react';
 
 const Routers = (): JSX.Element => {
-  const { silentLogin, saveLoginUserInfo } = useLogin();
+  const { useSilentLogin } = useLogin();
+  const isOAuth = useRecoilValue(OAuthState);
+  const setAppComponentDidMount = useSetRecoilState(AppComponentMountState);
 
-  let isOAuth = false;
-
-  if (localStorage.getItem('Authentication')) {
-    const isLogined = silentLogin();
-    if (isLogined) isOAuth = true;
-  }
+  useSilentLogin();
 
   useEffect(() => {
-    if (isOAuth) {
-      saveLoginUserInfo();
-    }
+    setAppComponentDidMount(true);
   }, []);
 
   return (
