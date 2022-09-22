88a4eecb65609bef1a4f2a3801bc92259d8fc5a5
jm <jm.dev.study@gmail.com>
2022-09-23 03:48:57 +0900

    ♻️ 이슈 디테일의 코멘트와 히스토리를 시간순으로 배치하도록 수정 - 타입 가드를 사용해서 타입에 따라 해당하는 컴포넌트가 렌더링 되도록 함

--- src/pages/Private/IssueDetail/index.tsx
+++ src/pages/Private/IssueDetail/index.tsx
@@ -1,8 +1,10 @@
+/* eslint-disable array-callback-return */
 import React, { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { useParams } from 'react-router-dom';
 import { LoginUserInfoState } from '@/stores/loginUserInfo';
 import useFetchIssue from '@/api/issue/useFetchIssue';
+import { isIssueCommentsTypes, isIssueHistoryTypes } from '@/api/issue/types';
 
 import * as S from '@/pages/Private/IssueDetail/index.styled';
 import Button from '@/components/Atoms/Button';
@@ -12,6 +14,7 @@ import UserImage from '@/components/Atoms/UserImage';
 import Comment from '@/components/Molecules/Comment';
 import IssueHeader from '@/components/Organisms/IssueHeader';
 import IsssueDetailAside from '@/pages/Private/IssueDetail/Aside';
+import IssueHistory from '@/pages/Private/IssueDetail/History';
 
 import { DEFAULT_TEXTAREA_MAX_LENGTH } from '@/components/Molecules/TextAreaEditer/constants';
 
@@ -21,7 +24,7 @@ const IssueDetail = (): JSX.Element => {
   const { data: issue } = useIssueData(Number(issueId));
   const { mutate: addIssueComment } = useAddIssueComment(Number(issueId));
 
-  const { id, closed, title, createdAt, lastModifiedAt, author, comments } = issue!;
+  const { id, closed, title, createdAt, lastModifiedAt, author, comments, issueHistories } = issue!;
 
   const userInfo = useRecoilValue(LoginUserInfoState);
   const memberId = userInfo.id;
@@ -46,6 +49,28 @@ const IssueDetail = (): JSX.Element => {
     return setTextAreaValue(value);
   };
 
+  const TimeLine = [...comments, ...issueHistories];
+  const sortTimeLine = TimeLine.sort((a, b) => {
+    const timeStampToNumber = (timeStamp: string) => Number(new Date(timeStamp));
+    if (isIssueCommentsTypes(a) && isIssueHistoryTypes(b)) {
+      return timeStampToNumber(a.createdAt) - timeStampToNumber(b.modifiedAt);
+    }
+
+    if (isIssueCommentsTypes(a) && isIssueCommentsTypes(b)) {
+      return timeStampToNumber(a.createdAt) - timeStampToNumber(b.createdAt);
+    }
+
+    if (isIssueHistoryTypes(a) && isIssueCommentsTypes(b)) {
+      return timeStampToNumber(a.modifiedAt) - timeStampToNumber(b.createdAt);
+    }
+
+    if (isIssueHistoryTypes(a) && isIssueHistoryTypes(b)) {
+      return timeStampToNumber(a.modifiedAt) - timeStampToNumber(b.modifiedAt);
+    }
+
+    return 0;
+  });
+
   return (
     <>
       <IssueHeader
@@ -59,20 +84,25 @@ const IssueDetail = (): JSX.Element => {
       />
       <S.IssueContent>
         <S.IssueComments>
-          {comments.map((comment) => {
-            const isCommentAuthor = memberId === comment.author.id;
-
-            return (
-              <S.Comment key={comment.id}>
-                <UserImage {...comment.author} imgSize="MEDIUM" />
-                <Comment
-                  issueId={id}
-                  isAuthor={isCommentAuthor}
-                  comment={comment}
-                  setSelectCommentId={setSelectCommentId}
-                />
-              </S.Comment>
-            );
+          {sortTimeLine.map((content, index) => {
+            if (isIssueCommentsTypes(content)) {
+              const isCommentAuthor = memberId === content.author.id;
+              return (
+                <S.Comment key={content.id}>
+                  <UserImage {...content.author} imgSize="MEDIUM" />
+                  <Comment
+                    issueId={id}
+                    isAuthor={isCommentAuthor}
+                    comment={content}
+                    setSelectCommentId={setSelectCommentId}
+                  />
+                </S.Comment>
+              );
+            }
+            if (isIssueHistoryTypes(content)) {
+              // eslint-disable-next-line react/no-array-index-key
+              return <IssueHistory key={`history_${index}`} {...content} />;
+            }
           })}
           <S.NewComment>
             <UserImage {...userInfo} imgSize="MEDIUM" />
